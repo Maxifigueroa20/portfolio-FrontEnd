2a6c060adcba3030c0f2cb01bf6f80ed18b341d5
Maximiliano <marianof0101@gmail.com>
2023-03-10 00:04:42 -0300

    Configuraciones iniciales

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { NgCircleProgressModule } from 'ng-circle-progress';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({
+      radius: 50,
+      outerStrokeWidth: 6,
+      outerStrokeColor: "#ffffff",
+      lazy: true,
+      showInnerStroke: false,
+      showImage: true,
+      imageHeight: 70,
+      imageWidth:70,
+      renderOnClick: false,
+      animationDuration: 700
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
