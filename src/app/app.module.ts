2e280cf6cba2b8012588e0d11fa4750b8c6a3029
Maximiliano <marianof0101@gmail.com>
2023-03-21 05:53:32 -0300

    Creación de todos los componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,28 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { NgCircleProgressModule } from 'ng-circle-progress';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { HabilidadComponent } from './components/habilidad/habilidad.component';
+import { ProyectoComponent } from './components/proyecto/proyecto.component';
+import { LoginComponent } from './components/login/login.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PortfolioComponent,
+    AcercaDeComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+    HabilidadComponent,
+    ProyectoComponent,
+    LoginComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
